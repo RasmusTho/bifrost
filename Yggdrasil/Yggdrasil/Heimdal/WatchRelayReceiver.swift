0bb0c70b961abcc18536f0440ea6e42150f4da97
Rasmus Thornberg <rasmus.thornberg@gmail.com>
2026-07-21 14:53:03 +0200

    Keep audio custody ahead of optional metadata

--- Yggdrasil/Yggdrasil/Heimdal/WatchRelayReceiver.swift
+++ Yggdrasil/Yggdrasil/Heimdal/WatchRelayReceiver.swift
@@ -52,14 +52,9 @@ final class WatchRelayFileStager {
         let destinationURL = stagingDirectory.appendingPathComponent(
             "watch-\(UUID().uuidString.lowercased()).m4a"
         )
+        try fileManager.copyItem(at: fileURL, to: destinationURL)
         if let metadata {
             try metadataStore.write(metadata, for: destinationURL)
-        }
-        do {
-            try fileManager.copyItem(at: fileURL, to: destinationURL)
-        } catch {
-            try? metadataStore.remove(for: destinationURL)
-            throw error
         }
         return destinationURL
     }
